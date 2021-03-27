859c8e7140ed7674aacfa898bde3ae4e8b9bd449
Natalia <nataliasyafitrik@gmail.com>
2021-03-27 11:29:54 +0700

    Add bash script for task 2

---
+++ task2-install-nginx.sh
@@ -0,0 +1,11 @@
+sudo su - #get root access
+apt-get update #update OS
+apt-get install nginx -y #install NGINX, -y meanes "yes" as answer to all prompts
+ps auwx | grep nginx #confirm nginx running
+#a = show processes for all users, u = display the process's user/owner, x = also show processes not attached to a terminal
+#to see webpage click the External IP link in the row
+
+
+
+
+
